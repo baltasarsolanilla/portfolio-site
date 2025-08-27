0b6faf5c7926eb5b7a5d018d80dce5ce00f44f11
Balti Solanilla <solanilla.baltasar@gmail.com>
2025-08-27 11:12:52 +1000

    Hide main-hero buttons (download CV and work)

--- components/main-hero/main-hero.tsx
+++ components/main-hero/main-hero.tsx
@@ -9,7 +9,7 @@ export default function MainHero() {
       <p className="text-xl md:text-2xl text-gray-300 mb-8 max-w-3xl mx-auto">
         Front-end Engineer - Known for improving technical efficiences
       </p>
-      <div className="flex flex-col sm:flex-row gap-4 justify-center">
+      <div className="hidden flex flex-col sm:flex-row gap-4 justify-center">
         <Button size="lg" className="bg-purple-600 hover:bg-purple-700">
           TODO: Ver mi trabajo
         </Button>
