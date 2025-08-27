4b003b4b309a3654eede775bca690dd52802c27e
Balti Solanilla <solanilla.baltasar@gmail.com>
2025-08-27 10:53:41 +1000

    Hide navbar for time being - pages still accessible by hardcoding pathname

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -6,7 +6,8 @@ export default function MainHeader() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="text-xl font-bold">Baltasar Solanilla</div>
-          <div className="hidden md:flex space-x-8">
+          {/* Hide navbar for the time being - add when back: md:flex */}
+          <div className="hidden space-x-8">
             <HeaderNavBar />
           </div>
         </div>
