f0ecb4409117f65e3e6bccd9e325f0114eead5d4
Balti Solanilla <solanilla.baltasar@gmail.com>
2025-08-29 12:53:04 +1000

    Trying out module resolution issue

--- types/images.d.ts
+++ types/images.d.ts
@@ -1,52 +1,69 @@
-// For Next.js projects using StaticImageData
-import { StaticImageData } from 'next/image';
-
 declare module '*.png' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.jpg' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.jpeg' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.gif' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.svg' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.webp' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.ico' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.bmp' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.tiff' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
 
 declare module '*.avif' {
+  import { StaticImageData } from 'next/image';
   const value: StaticImageData;
+  export = value;
   export default value;
 }
